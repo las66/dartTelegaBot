83d452581a0d2a3e3cfc6f8eab083450b5d4d97f
LyadovAS <las6@mail.ru>
2022-03-07 03:35:59 +0300

    init commit

---
+++ bin/dartTelegaBot.dart
@@ -0,0 +1,17 @@
+import 'dart:io';
+
+import 'package:teledart/teledart.dart';
+import 'package:teledart/telegram.dart';
+
+void main() async {
+  final botToken = await File('../bot_token.txt').readAsString();
+  final username = (await Telegram(botToken).getMe()).username;
+
+  var teledart = TeleDart(botToken, Event(username!));
+
+  teledart.start();
+
+  teledart
+      .onCommand(RegExp('hello', caseSensitive: false))
+      .listen((message) => message.reply('hi!'));
+}
