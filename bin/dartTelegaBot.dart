56b49a06ee0227ac0b3fb52bc2ba7f8cf156db7e
LyadovAS <las6@mail.ru>
2022-03-09 20:31:24 +0300

    commit for test jenkins job

--- bin/dartTelegaBot.dart
+++ bin/dartTelegaBot.dart
@@ -13,5 +13,5 @@ void main() async {
 
   teledart
       .onCommand(RegExp('hello', caseSensitive: false))
-      .listen((message) => message.reply('hello2!'));
+      .listen((message) => message.reply('hello3!'));
 }
